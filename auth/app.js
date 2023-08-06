f4b9bb4f0e6941017fa3ea620079bdf58e462bea
Cherpatel <zakrikz@gmail.com>
2023-08-06 17:09:44 +0200

    initial commit

---
+++ auth/app.js
@@ -0,0 +1,100 @@
+﻿import Express from "express";
+import cookieParser from "cookie-parser";
+
+import jwt from "jsonwebtoken"
+import md5 from "md5";
+import fs from "node:fs"
+
+const app = Express();
+
+const availableArgs = ["--private", "--public", "--port"]
+const args = process.argv;
+const flags = {
+    private: "",
+    public: "",
+    port: 8091
+};
+
+for (let i = 0; i < args.length; i++) {
+    if (availableArgs.includes(args[i]))
+        flags[args[i].slice(2)] = args[++i];
+}
+
+const cookieName = "jwt";
+const privateKey = fs.readFileSync(flags.private);
+const publicKey = fs.readFileSync(flags.public);
+
+const db = {
+    users: {}
+};
+
+app.use(cookieParser());
+app.use(Express.json({
+    type(req) {
+      return true;
+    }
+}));
+
+const checkUserCookie = (req, res, next) => {
+    if (req.cookies[cookieName] === undefined)
+        return res.status(401).end();
+    
+    const cookieValue = req.cookies[cookieName];
+    
+    jwt.verify(cookieValue, publicKey, (err, decode) => {
+        if (err || !decode.username || !decode.password || !(decode.username in db.users)) {
+            res.cookie(cookieName, '', { maxAge: -1 });
+            return res.status(400).end();
+        }
+
+        req.profile = decode;
+        next();
+    });
+}
+
+app.post("/signup", async (req, res) => {
+    const {
+        username,
+        password
+    } = req.body;
+
+    if (!username || !password)
+        return res.status(400).end();
+
+    if (db.users[username])
+        return res.status(403).end();
+    
+    const hashPassword = md5(md5(password) + username);
+    db.users[username] = hashPassword;
+    
+    const jwtToken = jwt.sign({ username, password: hashPassword }, privateKey, { algorithm: "RS256" });
+    res.cookie(cookieName, jwtToken);
+    res.status(200).end();
+});
+
+app.post("/login", (req, res) => {
+    const {
+        username,
+        password
+    } = req.body;
+
+    if (username === undefined || password === undefined)
+        return res.status(400).end();
+    
+    if (db.users[username] !== md5(md5(password) + username))
+        return res.status(403).end();
+
+    const jwtToken = jwt.sign({ username }, privateKey, { algorithm: "RS256" })
+    res.cookie(cookieName, jwtToken);
+    res.status(200).end();
+});
+
+app.get("/whoami", checkUserCookie, (req, res) => {
+    res.send(`Hello, ${req.profile.username}`);
+});
+
+app.listen(flags.port, () => {
+    console.log(`Service start ${flags.port} port`);
+    console.log(`Private key path ${flags.private}`);
+    console.log(`Public key path ${flags.public}`);
+});
---
+++ kv/app.js
@@ -0,0 +1,83 @@
+﻿import Express from "express";
+import cookieParser from "cookie-parser";
+
+import jwt from "jsonwebtoken"
+import fs from "node:fs"
+
+const app = Express();
+
+const availableArgs = ["--private", "--public", "--port"]
+const args = process.argv;
+const flags = {
+    public: "",
+    port: 8091
+};
+
+for (let i = 0; i < args.length; i++) {
+    if (availableArgs.includes(args[i]))
+        flags[args[i].slice(2)] = args[++i];
+}
+
+const cookieName = "jwt";
+const publicKey = fs.readFileSync(flags.public);
+
+const db = {
+    storage: {}
+};
+
+app.use(cookieParser());
+app.use(Express.json({
+    type(req) {
+      return true;
+    }
+}));
+
+const checkUserCookie = (req, res, next) => {
+    if (!req.cookies[cookieName])
+        return res.status(401).end();
+    
+    const cookieValue = req.cookies[cookieName];
+    
+    jwt.verify(cookieValue, publicKey, (err, decode) => {
+        if (err || !decode.username || !decode.password) {
+            res.cookie(cookieName, '', { maxAge: -1 });
+            return res.status(400).end();
+        }
+
+        req.profile = decode;
+        next();
+    });
+}
+
+app.post("/put", checkUserCookie, (req, res) => {
+    const { key } = req.query;
+    const { value } = req.body;
+    
+    if (!key || !value)
+        return res.status(400).end();
+
+    if (db.storage[key] && db.storage[key].login !== req.profile.username)
+        return res.status(403).end();
+    
+    db.storage[key] = { login: req.profile.username, value };
+    res.status(200).end();
+});
+
+app.get('/get', checkUserCookie, (req, res) => {
+    const { key } = req.query;
+
+    if (!key)
+        return res.status(400).end();
+    
+    if (!db.storage[key])
+        return res.status(404).end();
+
+    if (db.storage[key].login !== req.profile.username)
+        return res.status(403).end();
+    
+    res.send({ value: db.storage[key].value });
+});
+
+app.listen(flags.port, () => {
+    console.log(`Service start ${flags.port} port`);
+});
